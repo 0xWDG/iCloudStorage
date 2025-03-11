a4a5d7b335d7c89457dafa22ed88f06bc0db9915
Wesley de Groot <email@wesleydegroot.nl>
2025-03-11 20:58:40 +0100

    Update value if synced from remote

--- Sources/iCloudStorage/iCloudStorage.swift
+++ Sources/iCloudStorage/iCloudStorage.swift
@@ -12,6 +12,7 @@
 #if canImport(SwiftUI)
 import Foundation
 import SwiftUI
+import Combine
 
 /// A property wrapper that reads and writes to iCloud.
 ///
@@ -28,7 +29,21 @@ public struct iCloudStorage<T>: DynamicProperty {
     /// The default value to use if the value is not set yet.
     let defaultValue: T
 
-    @State private var value: T
+    var cancellables = Set<AnyCancellable>()
+
+    final private class Storage: ObservableObject {
+        var value: T {
+            willSet {
+                objectWillChange.send()
+            }
+        }
+
+        init(_ value: T) {
+            self.value = value
+        }
+    }
+
+    @ObservedObject private var value: Storage
 
     /// Creates an `iCloudStorage` property.
     ///
@@ -37,18 +52,34 @@ public struct iCloudStorage<T>: DynamicProperty {
     public init(wrappedValue: T, _ key: String) {
         self.key = key
         self.defaultValue = wrappedValue
-        self.value = NSUbiquitousKeyValueStore.default.object(forKey: key) as? T ?? defaultValue
+        self.value = Storage(
+            NSUbiquitousKeyValueStore.default.object(forKey: key) as? T ?? defaultValue
+        )
+
+        NotificationCenter.default.publisher(
+            for: NSUbiquitousKeyValueStore.didChangeExternallyNotification,
+            object: NSUbiquitousKeyValueStore.default
+        )
+        .receive(on: DispatchQueue.main)
+        .sink { [self] notification in
+            if let keys = notification.userInfo?[NSUbiquitousKeyValueStoreChangedKeysKey] as? [String],
+               keys.contains(key) {
+                self.value.value = NSUbiquitousKeyValueStore.default.object(forKey: key) as? T ?? defaultValue
+                self.value.objectWillChange.send()
+            }
+        }
+        .store(in: &cancellables)
     }
 
     /// The value of the key in iCloud.
     public var wrappedValue: T {
         get {
-            return value
+            return value.value
         }
 
         nonmutating set {
             NSUbiquitousKeyValueStore.default.set(newValue, forKey: key)
-            value = newValue
+            value.value = newValue
         }
     }
 
@@ -57,6 +88,7 @@ public struct iCloudStorage<T>: DynamicProperty {
         Binding {
             return self.wrappedValue
         } set: { newValue in
+            value.value = newValue
             self.wrappedValue = newValue
         }
     }
